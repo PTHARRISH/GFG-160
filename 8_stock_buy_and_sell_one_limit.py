3cc032e10ac83e919adfa691c846339aaa64f7b6
PTHARRISH <harrishpt@gmail.com>
2025-09-08 23:45:23 +0530

    Missing line of code

--- 8_stock_buy_and_sell_one_limit.py
+++ 8_stock_buy_and_sell_one_limit.py
@@ -8,11 +8,13 @@ def maximumProfit(prices):
         res = max(res, (prices[i] - min_value))
     return res
 
+# time complexity: O(n)
+# Space Complexity: O(1)
 
 def maximumProfit(prices):
     res = 0
     n = len(prices)
-    for i in range(n + 1):
+    for i in range(n):
         for j in range(i + 1, n):
             res = max(res, prices[j] - prices[i])
     return res
